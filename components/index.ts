6ef96e7d299257510667d0f931cd50c5569488c4
Anton Tapper Dock <at@appsson.se>
2023-09-11 19:58:48 +0200

    created combobox / searchbar

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,15 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter
 }
